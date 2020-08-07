86842d5e5eedd49b513eec54b7d9453ff985cb83
Tobias Golbs <golbs@e-spirit.com>
2020-08-07 13:28:18 +0200

    Change key of TeaserSection to teaser

--- src/components/Section/components/index.ts
+++ src/components/Section/components/index.ts
@@ -2,7 +2,7 @@ import TeaserSection from "./TeaserSection";
 import InterestingFactsSection from "./InterestingFactsSection";
 
 export default {
-  welcome: TeaserSection,
+  teaser: TeaserSection,
   // eslint-disable-next-line
   interesting_facts: InterestingFactsSection,
 };
