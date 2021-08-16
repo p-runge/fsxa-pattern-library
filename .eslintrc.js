61d1610d2b9bd6a6bcd176d49a8c12612a75a807
Dominik Barczewski <68270882+dbarczewski@users.noreply.github.com>
2021-08-16 06:14:46 +0200

    fix: update dependencies (#15)

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-console": "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "comma-dangle": ["error", "always-multiline"],
     "@typescript-eslint/camelcase": "off",
